f1d8d2f12e9424fdb91c5746789bbdf9325a5030
Vincenzo <vincenzo.ventriglia@outlook.com>
2023-06-23 16:30:26 +0200

    add sunburst diagram

--- src/pages/1_📈_Basic.py
+++ src/pages/1_📈_Basic.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
 from var import GLOBAL_STREAMLIT_STYLE, FAVICON
 from utils import aggregate_by_ticker, get_last_closing_price
+from plot import plot_sunburst
 
 st.set_page_config(
     page_title="PFN | Basic",
@@ -14,9 +13,9 @@ st.set_page_config(
 
 st.markdown(GLOBAL_STREAMLIT_STYLE, unsafe_allow_html=True)
 
-
 if "data" in st.session_state:
     df_storico = st.session_state["data"]
+    df_anagrafica = st.session_state["dimensions"]
 else:
     st.error(
         "Ops... non c'è niente da visualizzare. Passa prima per la 🏠 per caricare i dati"
@@ -37,18 +36,44 @@ fees = df_storico["fees"].sum().round(2)
 
 st.markdown("## Profit and Loss")
 
-pf_actual_value = (df_j["shares"] * df_j["price"]).sum()
-pnl = (pf_actual_value - expense).round(2)
-# pnl_perc =
-# pnl_fees =
-# pnl_fees_perc =
+col_l, col_r = st.columns([1, 1], gap="small")
 
-st.metric(
-    label="Actual PF Value", value=f"{np.round(pf_actual_value, 2)} €", delta=f"{pnl} €"
+consider_fees = st.checkbox("Take fees into account")
+
+pf_actual_value = (df_j["shares"] * df_j["price"]).sum()
+if consider_fees:
+    pnl = pf_actual_value - expense - fees
+    pnl_perc = 100 * (pf_actual_value - expense - fees) / expense
+else:
+    pnl = pf_actual_value - expense
+    pnl_perc = 100 * (pf_actual_value - expense) / expense
+sign = "+" if pnl >= 0 else "-"
+
+col_l.metric(
+    label="Actual Portfolio Value",
+    value=f"{pf_actual_value: .1f} €",
+    delta=f"{pnl: .1f} € ({sign}{pnl_perc: .1f}%)",
 )
 
-# np.round(100 * (pf_actual_value - expense) / expense, 1)
+df_j["ticker"] = df_j["ticker_yf"].str.split(".").str[0]
+df_j["position_value"] = df_j["shares"] * df_j["price"]
+df_pivot = (
+    df_j.merge(df_anagrafica, how="left", on="ticker")
+    .groupby(
+        [
+            "macro_asset_class",
+            "asset_class",
+            "ticker",
+            "name",
+        ]
+    )["position_value"]
+    .sum()
+    .reset_index()
+)
+df_pivot["weight_pf"] = (
+    (100 * df_pivot["position_value"].div(pf_actual_value)).astype(float).round(1)
+)
 
-# (pf_actual_value - expense - fees).round(2)
-
-# np.round(100 * (pf_actual_value - expense - fees) / expense, 1)
+st.markdown("## Portfolio asset allocation")
+fig = plot_sunburst(df=df_pivot)
+st.plotly_chart(fig, use_container_width=True)
---
+++ src/plot.py
@@ -0,0 +1,16 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import numpy as np
+
+
+def plot_sunburst(df: pd.DataFrame) -> go.Figure:
+    fig = px.sunburst(
+        data_frame=df.assign(hole=" "),
+        path=["hole", "macro_asset_class", "asset_class", "ticker"],
+        values="weight_pf",
+    )
+    fig.update_traces(hovertemplate="<b>%{value:.1f}%")
+    fig.update_layout(autosize=False, height=650, hoverlabel_font_size=14)
+
+    return fig
