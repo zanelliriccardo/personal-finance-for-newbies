081278cac1b3ac1bcdc7288cb36ce825b5203e4b
Vincenzo_Ventriglia <vventriglia@zanichelli.it>
2023-06-25 14:41:00 +0200

    mock data fix

--- src/plot.py
+++ src/plot.py
@@ -7,10 +7,9 @@ import numpy as np
 def plot_sunburst(df: pd.DataFrame) -> go.Figure:
     fig = px.sunburst(
         data_frame=df.assign(hole=" "),
-        path=["hole", "macro_asset_class", "asset_class", "ticker"],
+        path=["hole", "macro_asset_class", "asset_class", "ticker_yf"],
         values="weight_pf",
     )
     fig.update_traces(hovertemplate="<b>%{value:.1f}%")
     fig.update_layout(autosize=False, height=650, hoverlabel_font_size=14)
-
     return fig
