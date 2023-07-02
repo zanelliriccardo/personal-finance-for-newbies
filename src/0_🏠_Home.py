3687973b8f46a42dafd1d044b88282b5238c899c
Vincenzo <vincenzo.ventriglia@outlook.com>
2023-07-02 20:39:39 +0200

    added buttons and disclaimer in Home page

--- src/0_🏠_Home.py
+++ src/0_🏠_Home.py
@@ -26,10 +26,44 @@ st.markdown(
 )
 st.markdown("***")
 st.markdown("## Let's get started")
+st.markdown(
+    'You can <span style="color:green">download</span> a template, \
+    <span style="color:green">fill</span> it in with your accumulation plan\'s buy/sell transactions, \
+    and <span style="color:green">upload</span> it.',
+    unsafe_allow_html=True,
+)
+col_ll, col_l, col_c, col_r, col_rr = st.columns([0.6, 1, 0.6, 1, 0.6], gap="small")
+with open("../data/in/template.xlsx", "rb") as f:
+    col_l.download_button(
+        "Download Template",
+        data=f,
+        file_name="template.xlsx",
+        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    )
+col_r.button("Upload your data [WIP]")
 
-st.button(label="Load Mock Data", key="load_mock_df")
+st.markdown("##")
+
+st.markdown(
+    'If you wish to <span style="color:green">explore</span> the app first, \
+    upload some <span style="color:green">demo data</span> instead.',
+    unsafe_allow_html=True,
+)
+col_ll_mid, col_l_mid, col_c_mid, col_r_mid, col_rr_mid = st.columns(
+    [0.6, 1, 0.6, 1, 0.6], gap="small"
+)
+col_l_mid.button(label="Load Mock Data", key="load_mock_df")
 
 if st.session_state.get("load_mock_df"):
-    df_storico, df_anagrafica = load_data(DATA_PATH / Path("pac.xlsx"))
+    df_storico, df_anagrafica = load_data(DATA_PATH / Path("demo.xlsx"))
     st.session_state["data"] = df_storico
     st.session_state["dimensions"] = df_anagrafica
+
+st.markdown("***")
+st.markdown(
+    '<center> <span style="font-size:0.7em; font-style:italic">\
+    This content is for educational purposes only and is under no circumstances intended\
+    to be used or considered as financial or investment advice.\
+    </span> </center>',
+    unsafe_allow_html=True,
+)
