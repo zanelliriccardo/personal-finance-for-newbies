3db65fa75aa22f0b3b11fa230ee92c6df8de0959
Vincenzo_Ventriglia <vventriglia@zanichelli.it>
2023-07-09 21:18:13 +0200

    load from file now working

--- src/0_🏠_Home.py
+++ src/0_🏠_Home.py
@@ -27,10 +27,23 @@ st.markdown(
 st.markdown("***")
 st.markdown("## Let's get started")
 st.markdown(
-    "You can **download** a template, **fill** it in with your accumulation plan's buy/sell transactions, and **upload** it:",
+    "To load your data, fill in the template with your accumulation plan's buy/sell transactions and upload it here:",
     unsafe_allow_html=True,
 )
-col_ll, col_l, col_c, col_r, col_rr = st.columns([0.6, 1, 0.6, 1, 0.6], gap="small")
+col_l, col_r = st.columns([1, 0.8], gap="small")
+
+uploaded_file = col_l.file_uploader(
+    label="Upload your Data",
+)
+if uploaded_file is not None:
+    try:
+        df_storico, df_anagrafica = load_data(uploaded_file)
+        st.session_state["data"] = df_storico
+        st.session_state["dimensions"] = df_anagrafica
+    except:
+        st.error("Please check your file format and make sure it matches the template")
+        st.stop()
+
 with open(DATA_PATH / Path("demo.xlsx"), "rb") as f:
     col_l.download_button(
         "Download Template",
@@ -38,22 +51,14 @@ with open(DATA_PATH / Path("demo.xlsx"), "rb") as f:
         file_name="template.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     )
-if col_r.button("Upload your Data"):
-    uploaded_file = st.file_uploader(label="", label_visibility="collapsed")
-    if uploaded_file is not None:
-        col_r.write("You selected the file:", uploaded_file.name)
 
 st.markdown("##")
 st.markdown(
-    "If you wish to **explore** the app first, upload some **demo data** instead:",
+    "If you wish to **explore** the app first, load some **demo data** instead:",
     unsafe_allow_html=True,
 )
-col_ll_mid, col_l_mid, col_c_mid, col_r_mid, col_rr_mid = st.columns(
-    [0.6, 1, 0.6, 1, 0.6], gap="small"
-)
-col_l_mid.button(label="Load Mock Data", key="load_mock_df")
 
-if st.session_state.get("load_mock_df"):
+if st.button(label="Load Mock Data", key="load_mock_df"):
     df_storico, df_anagrafica = load_data(DATA_PATH / Path("demo.xlsx"))
     st.session_state["data"] = df_storico
     st.session_state["dimensions"] = df_anagrafica
